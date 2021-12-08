f963452fcb25dbfcaffd8a77541d3120ce5441a4
Juan Pablo Rodriguez Mendoza <pablordzmz@gmail.com>
2021-12-08 15:20:16 -0600

    Probando svelte dev javaScript

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EvelteDevTestV1
 Practica con otra libreria reactiva, lei documentacion
+
+### Como aprendi yo a instalarlo
+npx degit sveltejs/template my-proyect
+cd search-movies
+npm install
+npm run dev
---
+++ svelteTest2/search-movies/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name;
+	export let lastname;
+	// Alos 2 segundos, queremos que name pasae a ser frotener
+	setTimeout(function(){
+		name = 'frotener'
+	},2000)
+</script>
+
+<main>
+	<h1>Hola {name} {lastname}!</h1>
+	<p>Loco loco <a href="https://svelte.dev/tutorial">YEaaaaa</a> Pero esto es javascript, todo se ejecuta aqui</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
